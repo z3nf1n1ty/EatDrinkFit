39678f09da4afa2bd034a4ed78c49a342bffce99
Michael Peterson <14036481+z3nf1n1ty@users.noreply.github.com>
2024-03-19 12:25:32 -0400

    Database upgrade

---
+++ EatDrinkFit.Web/Data/Migrations/20240319162302_update 3.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EatDrinkFit.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class update3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "FromFavorites",
+                table: "MacroLogs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFavorite",
+                table: "MacroLogs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Source",
+                table: "MacroLogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FromFavorites",
+                table: "MacroLogs");
+
+            migrationBuilder.DropColumn(
+                name: "IsFavorite",
+                table: "MacroLogs");
+
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "MacroLogs");
+        }
+    }
+}
