c071d8f6874db6795515c2872a5317d9d8ca1409
Michael Peterson <14036481+z3nf1n1ty@users.noreply.github.com>
2024-03-23 00:03:02 -0400

    update to chart.js 4, update dashboard to work with the never chart.js version

--- EatDrinkFit.Web/wwwroot/js/site.js
+++ EatDrinkFit.Web/wwwroot/js/site.js
@@ -18,3 +18,54 @@ window.onload = function () {
     //setValue("ManualTimeZone", Intl.DateTimeFormat().resolvedOptions().timeZone);
     $(".hiddentimezone").val(Intl.DateTimeFormat().resolvedOptions().timeZone);
 }
+
+function dashboardCaloriesChart() {
+    var areaChartData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: '',
+                backgroundColor: 'rgba(255,193,7,0.6)',
+                borderColor: 'rgba(255,193,7,0.7)',
+                pointRadius: false,
+                pointColor: '#3b8bba',
+                pointStrokeColor: 'rgba(255,193,7,1)',
+                pointHighlightFill: '#fff',
+                pointHighlightStroke: 'rgba(255,193,7,1)',
+                data: [1718, 1621, 2235, 1864, 1892, 2137, 1789, 1934, 2013, 1876]
+            },
+
+        ]
+    }
+
+
+    var barChartCanvas = $('#CaloriesBarChart').get(0).getContext('2d')
+    var barChartData = $.extend(true, {}, areaChartData)
+    var temp0 = areaChartData.datasets[0]
+    var temp1 = areaChartData.datasets[1]
+    barChartData.datasets[0] = temp0
+    //barChartData.datasets[1] = temp1
+
+    var barChartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        datasetFill: false,
+        legend: {
+            display: false
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+    }
+
+    new Chart(barChartCanvas, {
+        type: 'bar',
+        data: barChartData,
+        options: barChartOptions,
+
+    })
+}
